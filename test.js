ec6945d97de0110410a4199c4c265789211f689b
Jacinto Callu <jacintocallu@gmail.com>
2015-08-31 17:41:32 -0700

    Update test

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
-var db = require('./index.js');
-var connStr = "postgresql://postgres@127.0.0.1:5432/vgp";
-var vgpdb = db(connStr);
-
-
-console.log("vgpdb",vgpdb);
+var Db = require('./index.js');
+var dbName = process.env.DB_NAME || 'postgres'
+var connStr = "postgresql://postgres@127.0.0.1:5432/"+dbName ;
+var db = Db(connStr);
+console.log(dbName,db);
